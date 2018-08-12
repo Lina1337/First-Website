6ff4b096175446db91dd1d7a3c827961b0cee4f6
Lina1337 <38934864+Lina1337@users.noreply.github.com>
2018-08-12 18:51:45 +1200

    Add files via upload

---
+++ website/js/script.js
@@ -0,0 +1,16 @@
+  <!-- // menu burger -->
+
+
+
+
+
+    <!-- // full page tab -->
+    /* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+} 
